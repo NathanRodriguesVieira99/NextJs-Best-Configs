62cceebec4563e01696d05c6614d0e5c0e0c60eb
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-01-15 17:21:33 -0300

    fix: update jest configuration and improve Home component tests

--- src/components/Home/index.test.tsx
+++ src/components/Home/index.test.tsx
@@ -1,18 +1,25 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+
 import Home from "./index";
+
 describe("HomeScreen Component", () => {
   it("should render Home with hello message", () => {
     render(<Home />);
-    screen.getByText("Hello berimbau");
+
+    const textHello = screen.getByText("Hello berimbau");
+    expect(textHello).toBeInTheDocument();
   });
+
   it("should change message on button click", () => {
     render(<Home />);
 
-    screen.getByText("vasco da gama");
+    const InitialMessageText = screen.getByText("vasco da gama");
+    expect(InitialMessageText).toBeInTheDocument();
 
     const button = screen.getByText(/change message/i);
     fireEvent.click(button);
 
-    screen.getByText(/new message!/i);
+    const newMessage = screen.getByText(/new message!/i);
+    expect(newMessage).toBeInTheDocument();
   });
 });
